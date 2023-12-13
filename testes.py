130810cff8952d229bcf8172494a13eda2f488f6
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-12-13 11:11:47 -0300

    test: Atualização arquivo testes.py

--- testes.py
+++ testes.py
@@ -14,3 +14,13 @@ def test_ola_mundo():
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"mensagem": "Olá mundo!"}
+
+
+def test_listar_produtos_status_code():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+
+def test_tamanho_da_lista_de_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
